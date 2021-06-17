8ec877f5488d19671e0bec2bf506fa3437ca8048
Timofey <tmelent@gmail.com>
2021-06-18 02:20:33 +0300

    gitgignore, web typing issues

---
+++ server/dev_constants/connections.ts
@@ -0,0 +1,6 @@
+export const CORS_ORIGIN = "http://localhost:3000";
+export const PORT = "4000";
+export const COOKIE_NAME = "qid";
+export const __prod__ = process.env.NODE_ENV === "production";
+export const FORGET_PASSWORD_PREFIX = 'forget-password:';
+export const JOIN_GAME_PREFIX = 'join-game:';
